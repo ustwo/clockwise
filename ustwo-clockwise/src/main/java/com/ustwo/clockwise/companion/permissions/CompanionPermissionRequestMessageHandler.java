b2a74ae9464284ace285cb643414ae34c5c29501
Mitch Todd <mitch@ustwo.com>
2015-11-18 15:58:08 -0600

    check for bad PermissionRequestItem

--- ustwo-clockwise/src/main/java/com/ustwo/clockwise/companion/permissions/CompanionPermissionRequestMessageHandler.java
+++ ustwo-clockwise/src/main/java/com/ustwo/clockwise/companion/permissions/CompanionPermissionRequestMessageHandler.java
@@ -34,40 +34,44 @@ public class CompanionPermissionRequestMessageHandler implements MessageReceived
             boolean justChecking = map.getBoolean(Constants.DATA_KEY_JUST_CHECKING);
             byte[] requestBytes = map.getByteArray(Constants.DATA_KEY_PERMISSION_REQUEST);
             PermissionsRequest request = PermissionsRequest.deserialize(requestBytes);
+            if(request != null) {
+                // if requesting silently, just get the results and return them instead of invoking the activity
+                if (request.shouldRequestSilently()) {
+                    HashMap<String, Boolean> companionPermissionResults = new HashMap<>();
+                    for (PermissionRequestItem requestItem : request.getRequestItems()) {
+                        if (requestItem.isWearable()) continue;
 
-            // if requesting silently, just get the results and return them instead of invoking the activity
-            if(request.shouldRequestSilently()) {
-                HashMap<String, Boolean> companionPermissionResults = new HashMap<>();
-                for(PermissionRequestItem requestItem : request.getRequestItems()) {
-                    if(requestItem.isWearable()) continue;
-
-                    for(String companionPermission : requestItem.getPermissions()) {
-                        if (ContextCompat.checkSelfPermission(mContext, companionPermission) == PackageManager.PERMISSION_GRANTED) {
-                            companionPermissionResults.put(companionPermission, true);
-                        } else {
-                            companionPermissionResults.put(companionPermission, false);
+                        for (String companionPermission : requestItem.getPermissions()) {
+                            if (ContextCompat.checkSelfPermission(mContext, companionPermission) == PackageManager.PERMISSION_GRANTED) {
+                                companionPermissionResults.put(companionPermission, true);
+                            } else {
+                                companionPermissionResults.put(companionPermission, false);
+                            }
                         }
                     }
-                }
 
-                DataMap dataMap = new DataMap();
-                dataMap.putLong(Constants.DATA_KEY_TIMESTAMP, System.currentTimeMillis());
-                ArrayList<DataMap> results = new ArrayList<>();
-                for(String permission : companionPermissionResults.keySet()) {
-                    DataMap resultMap = new DataMap();
-                    resultMap.putString(Constants.DATA_KEY_COMPANION_PERMISSION, permission);
-                    resultMap.putBoolean(Constants.DATA_KEY_COMPANION_PERMISSION_GRANTED, companionPermissionResults.get(permission));
-                    results.add(resultMap);
+                    DataMap dataMap = new DataMap();
+                    dataMap.putLong(Constants.DATA_KEY_TIMESTAMP, System.currentTimeMillis());
+                    ArrayList<DataMap> results = new ArrayList<>();
+                    for (String permission : companionPermissionResults.keySet()) {
+                        DataMap resultMap = new DataMap();
+                        resultMap.putString(Constants.DATA_KEY_COMPANION_PERMISSION, permission);
+                        resultMap.putBoolean(Constants.DATA_KEY_COMPANION_PERMISSION_GRANTED, companionPermissionResults.get(permission));
+                        results.add(resultMap);
+                    }
+                    dataMap.putDataMapArrayList(Constants.DATA_KEY_COMPANION_PERMISSION_RESULTS, results);
+                    apiHelper.putMessage(Constants.DATA_PATH_COMPANION_PERMISSION_RESPONSE, dataMap.toByteArray(), null);
+                    //apiHelper.putDataMap(Constants.DATA_PATH_COMPANION_PERMISSION_RESPONSE, dataMap, null);
+                } else {
+                    Intent i = new Intent(mContext.getApplicationContext(), PermissionRequestActivity.class);
+                    i.putExtra(PermissionRequestActivity.EXTRA_COMPANION_JUST_CHECKING, justChecking);
+                    i.putExtra(PermissionRequestActivity.EXTRA_PERMISSION_REQUEST, requestBytes);
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    mContext.getApplicationContext().startActivity(i);
                 }
-                dataMap.putDataMapArrayList(Constants.DATA_KEY_COMPANION_PERMISSION_RESULTS, results);
-                apiHelper.putMessage(Constants.DATA_PATH_COMPANION_PERMISSION_RESPONSE, dataMap.toByteArray(), null);
-                //apiHelper.putDataMap(Constants.DATA_PATH_COMPANION_PERMISSION_RESPONSE, dataMap, null);
             } else {
-                Intent i = new Intent(mContext.getApplicationContext(), PermissionRequestActivity.class);
-                i.putExtra(PermissionRequestActivity.EXTRA_COMPANION_JUST_CHECKING, justChecking);
-                i.putExtra(PermissionRequestActivity.EXTRA_PERMISSION_REQUEST, requestBytes);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.getApplicationContext().startActivity(i);
+                // send back empty data map if we receive request with bad data
+                apiHelper.putMessage(Constants.DATA_PATH_COMPANION_PERMISSION_RESPONSE, new DataMap().toByteArray(), null);
             }
         }
     }
